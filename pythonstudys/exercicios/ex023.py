9aa9790aba528f08371fc185e1948b6f241abbfb
Heitor Roberto <rheitor127@gmail.com>
2025-05-15 16:16:50 -0300

    Trabalhando com condições

--- pythonstudys/exercicios/ex023.py
+++ pythonstudys/exercicios/ex023.py
@@ -3,6 +3,15 @@
 # unidade: 4 dezena: 3 centena: 8 milhar: 1
 
 n1 = input('Digite um número de 0 a 9999: ')
-if n1.isnumeric():
-    lista_numero = n1.zfill(4)
-    print(lista_numero)
+n2 = n1.isnumeric()
+formatacao = n1.zfill(4)
+formatacao = formatacao.replace('', ' ')
+lista_numero = formatacao.split()
+
+if n2 == True and len(lista_numero) <= 4:
+    print(
+        f'Milhar: {lista_numero[-4]}\nCentena: {lista_numero[-3]}\nDezena: {lista_numero[-2]}\nUnidade: {lista_numero[-1]}'
+    )
+else:
+    print('Este erro é exibido quando o número passa de 9999 ou não é um número. Tente Novamente.')
+
